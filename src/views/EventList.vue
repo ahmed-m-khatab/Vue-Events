11c83c0eb8bb5c3237f3f06d164ffbe6ff8f5e2a
Ahmed khatab <ahmed.khatab@adamtravel.net>
2022-10-18 17:26:49 +0200

    initial commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,43 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="events">
+    <h1>Event for good</h1>
+
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard
+  },
+  data () {
+    return {
+      events: null
+    }
+  },
+  created () {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
